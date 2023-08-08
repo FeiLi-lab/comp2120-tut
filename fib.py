ee098971444200054dc02373981272809f9a9ebc
Hugo Heanly <u7119555@anu.edu.au>
2023-08-08 11:26:10 +1000

    FixedFib

--- fib.py
+++ fib.py
@@ -1,21 +1,29 @@
-def fib(n):
-  '''
-  When given a position, the function returns the fibonacci at that position in the sequence.
-  The zeroth number in the fibonacci sequence is 0. The first number is 1
-  Negative numbers should return None
-  TODO: Make the algorithm work for larger numbers or implement faster algorithms
-  '''
-  if n == 0:
-    return 0
-  elif n == 1:
-    return 1
-  else:
-    return fib(n - 1) + fib(n - 2)
+def fib(n, memo={}):
+    '''
+    When given a position, the function returns the fibonacci at that position in the sequence.
+    The zeroth number in the fibonacci sequence is 0. The first number is 1
+    Negative numbers should return None
+    TODO: Make the algorithm work for larger numbers or implement faster algorithms
+    '''
+    if n < 0:
+        return None
+
+    if n in memo:
+        return memo[n]
+
+    if n == 0:
+        memo[n] = 0
+    elif n == 1:
+        memo[n] = 1
+    else:
+        memo[n] = fib(n - 1, memo) + fib(n - 2, memo)
+
+    return memo[n]
 
 # Test cases
-assert(fib(1) == 1)
-assert(fib(21) == 10946)
-assert(fib(0) == 0)
-assert(fib(-1) == None)
+assert fib(1) == 1
+assert fib(21) == 10946
+assert fib(0) == 0
+assert fib(-1) is None
 
 print("Code ran successfully!")
